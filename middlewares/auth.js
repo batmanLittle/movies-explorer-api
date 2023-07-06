f376b2be24223a2af4f3d6c1d1b5006d991cde02
Ksenia Ivanova <fen10wa@yandex.ru>
2023-07-06 22:50:40 +0300

    Обвновила огружение

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,10 +1,7 @@
 const jwt = require("jsonwebtoken");
 const Auth = require("../utils/Auth");
 
-// const { NODE_ENV, JWT_SECRET } = process.env;
-const NODE_ENV = "production";
-const JWT_SECRET =
-  "eb28135ebcfc17578f96d4d65b6c7871f2c803be4180c165061d5c2db621c51bl";
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
